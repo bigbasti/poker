69d5167b52f59ab8420f9a2ec64528168d4903e0
Sebastian Gross <s.gross@t-systems.com>
2021-05-17 12:12:45 +0200

    api project gradle configuration

---
+++ api/src/main/java/com/bigbasti/poker/api/PokerWebApiApplication.java
@@ -0,0 +1,22 @@
+package com.bigbasti.poker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableCaching
+@SpringBootApplication
+@ComponentScan(basePackages = "com.bigbasti.poker")
+@EntityScan(basePackages = "com.bigbasti.poker.data.dao")
+@EnableJpaRepositories(basePackages = {"com.bigbasti.poker.data.repository"})
+public class PokerWebApiApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokerWebApiApplication.class, args);
+	}
+
+}
