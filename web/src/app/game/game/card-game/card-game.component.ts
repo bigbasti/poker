74f3f7af2a387652678fd337b409a48f6b965cc5
Sebastian Gross <s.gross@t-systems.com>
2021-07-05 14:28:37 +0200

    own component for card-game type

--- web/src/app/game/game/card-game/card-game.component.ts
+++ web/src/app/game/game/card-game/card-game.component.ts
@@ -10,73 +10,75 @@ import {Router} from "@angular/router";
 import * as GameActions from "../../state/game.actions";
 
 @Component({
-  selector: 'poker-card-game',
-  template: `
-    <div class="row" *ngIf="vm$ | async as vm">
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col"></div>
-      <div class="col">
-        <ng-container *ngIf="!vm.round">
-          <button class="btn btn-primary" (click)="startNextRound()">Runde {{vm.game.gameRounds + 1}} Starten</button>
-        </ng-container>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col" *ngFor="let p of vm.game.players">
-        <div class="row">
-          {{p.user.name}}<br />
-          {{p.money}}
-        </div>
-        <div class="row">
+    selector: 'poker-card-game',
+    template: `
+        <ng-container *ngIf="vm$ | async as vm">
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col"></div>
+            <div class="col">
+                <ng-container *ngIf="!vm.round">
+                    <button class="btn btn-primary" (click)="startNextRound()">Runde {{vm.game.gameRounds + 1}}
+                        Starten
+                    </button>
+                </ng-container>
+            </div>
+            <div class="row">
+                <div class="col" *ngFor="let p of vm.game.players">
+                    <div class="row">
+                        {{p.user.name}}<br/>
+                        {{p.money}}
+                    </div>
+                    <div class="row">
 
-        </div>
-      </div>
-    </div>
-  `,
-  styles: [
-  ]
+                    </div>
+                </div>
+            </div>
+        </ng-container>
+    `,
+    styles: []
 })
 export class CardGameComponent implements OnInit, OnDestroy {
-  onDestroy$ = new Subject();
+    onDestroy$ = new Subject();
 
-  currentUser$ = this.store.select(getUser);
-  currentGame$ = this.store.select(getCurrentGame);
+    currentUser$ = this.store.select(getUser);
+    currentGame$ = this.store.select(getCurrentGame);
 
-  currentRound$ = this.currentGame$.pipe(
-      takeWhile(game => game !== null),
-      map(game => game.rounds.find(r => r.finished === false))
-  );
+    currentRound$ = this.currentGame$.pipe(
+        takeWhile(game => game !== null),
+        map(game => game.rounds.find(r => r.finished === false))
+    );
 
-  vm$ = combineLatest([this.currentGame$, this.currentUser$, this.currentRound$]).pipe(
-      map(([game, user, round]) => ({game, user, round}))
-  );
+    vm$ = combineLatest([this.currentGame$, this.currentUser$, this.currentRound$]).pipe(
+        map(([game, user, round]) => ({game, user, round}))
+    );
 
-  constructor(
-      private gameService: PokerGameService,
-      private store: Store<PokerState>,
-      private router: Router
-  ) { }
+    constructor(
+        private gameService: PokerGameService,
+        private store: Store<PokerState>,
+        private router: Router
+    ) {
+    }
 
-  ngOnInit(): void {
-    interval(5000).pipe(
-        tap(() => console.log("updating game state")),
-        tap(() => this.store.dispatch(GameActions.loadCurrentGame())),
-        takeUntil(this.onDestroy$)
-    ).subscribe();
-    this.store.dispatch(GameActions.loadCurrentGame());
-  }
+    ngOnInit(): void {
+        interval(5000).pipe(
+            tap(() => console.log("updating game state")),
+            tap(() => this.store.dispatch(GameActions.loadCurrentGame())),
+            takeUntil(this.onDestroy$)
+        ).subscribe();
+        this.store.dispatch(GameActions.loadCurrentGame());
+    }
 
-  ngOnDestroy(): void {
-    this.onDestroy$.next(null);
-  }
+    ngOnDestroy(): void {
+        this.onDestroy$.next(null);
+    }
 
-  startNextRound() {
-    this.store.dispatch(GameActions.startNextRound());
-  }
+    startNextRound() {
+        this.store.dispatch(GameActions.startNextRound());
+    }
 
 }
